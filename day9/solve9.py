b15e41106c21594506ce658a17b9cca1d4c9b092
s17kf <s17kf@users.noreply.github.com>
2022-12-09 08:05:02 +0100

    day 9 done

--- day9/solve9.py
+++ day9/solve9.py
@@ -18,50 +18,36 @@ input_file_name = script_arguments["inputFile"]
 print("solving file: " + input_file_name)
 input_lines = common.read_lines_from_file(input_file_name)
 
-table = [ [ 0 for y in range( 2000 ) ] for x in range( 2000 ) ]
-
-head = [999, 999]
-tail = [999, 999]
-table[tail[0]][tail[1]] = 1
-# print(input_lines)
-print(head, tail)
+knots = [[0, 0] for i in range(10)]
+k1, k9 = {(0, 0)}, {(0, 0)}
+DIRECTIONS = {
+    'R': (0, 1),
+    'L': (0, -1),
+    'D': (1, 0),
+    'U': (-1, 0)
+}
 
 for move in input_lines:
     direction, length = move.split()
-    # print(direction, length)
     length = int(length)
-    match direction:
-        case 'R':
-            head[1] += length
-        case 'L':
-            head[1] -= length
-        case 'D':
-            head[0] += length
-        case 'U':
-            head[0] -= length
-        case _:
-            print(f"unsupported move '{direction}'")
-    # print(abs(head [0] - tail[0]) > 1, abs(head[1] - tail[1] > 1))
-    while abs(head [0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
-        if head[0] - tail[0] > 0:
-            tail[0] += 1
-        elif head[0] - tail[0] < 0:
-            tail[0] -= 1
-        if head[1] - tail[1] > 0:
-            tail[1] += 1
-        elif head[1] - tail[1] < 0:
-            tail[1] -= 1
-        table[tail[0]][tail[1]] = 1
-        # print(head, tail)
+    for _ in range(length):
+        knots[0] = [knots[0][0] + DIRECTIONS[direction][0], knots[0][1] + DIRECTIONS[direction][1]]
+        for head in range(9):
+            tail = head + 1
+            if abs(knots[head][0] - knots[tail][0]) > 1 or abs(knots[head][1] - knots[tail][1]) > 1:
+                if knots[head][0] - knots[tail][0] > 0:
+                    knots[tail][0] += 1
+                elif knots[head][0] - knots[tail][0] < 0:
+                    knots[tail][0] -= 1
+                if knots[head][1] - knots[tail][1] > 0:
+                    knots[tail][1] += 1
+                elif knots[head][1] - knots[tail][1] < 0:
+                    knots[tail][1] -= 1
+        k1.add(tuple(knots[1]))
+        k9.add(tuple(knots[9]))
 
-# for row in table:
-#     print(row)
-
-print(head)
-print(tail)
-
-result1 = sum([sum(row) for row in table])
-result2 = 1
+result1 = len(k1)
+result2 = len(k9)
 
 print(f"task1: {result1}")
 print(f"task2: {result2}")
